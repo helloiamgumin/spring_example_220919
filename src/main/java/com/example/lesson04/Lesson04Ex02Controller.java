66f74b09c0d2d4ce5c4e6339016cd55e4b7947c9
rnjsrbals2 <rnjsrbals2@naver.com>
2022-12-28 22:01:32 +0900

    12-28 과제

--- src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -1,17 +1,22 @@
 package com.example.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.lesson04.bo.StudentBO;
 import com.example.lesson04.model.Student;
 
 @RequestMapping("/lesson04/ex02")
 @Controller
 public class Lesson04Ex02Controller {
 
+	@Autowired
+	private StudentBO studentBO;
+	
 	// 학생 추가 화면
 	// http://localhost/lesson04/ex02/add_student_view
 	@GetMapping("/add_student_view")
@@ -25,6 +30,7 @@ public class Lesson04Ex02Controller {
 			) {
 		
 		// db insert
+		studentBO.addStudent(student);
 		
 		// db select(방금 가입된 사람)
 		
